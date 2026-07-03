70b13ad9b761498330e0788c15729a4e7e2b06fc
ardince <52309954+ardince@users.noreply.github.com>
2026-07-03 13:16:52 +0300

    Add ForecastPipeline to module exports

--- batteryhealthforecast/__init__.py
+++ batteryhealthforecast/__init__.py
@@ -5,5 +5,10 @@ A modular scientific framework for battery degradation forecasting.
 """
 
 from .version import VERSION
+from .pipeline import ForecastPipeline
 
 __version__ = VERSION
+
+__all__ = [
+    "ForecastPipeline",
+]
